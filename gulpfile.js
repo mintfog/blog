c8f7f2166155385bec3e1201ae14e8975343c988
无名 <fdong26@gmail.com>
2022-05-18 21:51:08 +0800

    gulp bug fix

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ var htmlclean = require('gulp-htmlclean');
 
 // 压缩html文件
 gulp.task('minify-html', function (done) {
-    return gulp.src('./public/*.html')
+    return gulp.src('./public/**/*.html')
+	    .pipe(htmlclean())
 		.pipe(htmlmin({
             removeComments: true,
             minifyJS: true,
             minifyCSS: true,
             minifyURLs: true,
         }))
-		.pipe(htmlclean())
         .pipe(gulp.dest('./public'));
     done();
 });
